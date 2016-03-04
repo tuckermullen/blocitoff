04b1279db3231c145dad67199913e642feb20554
Tucker Mullen <mullentucker@gmail.com>
2016-03-04 11:15:24 -0800

    In progress 3/4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,18 @@ class ItemsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @items = @user.items
-    @item = current_user.items.build(item_params)
-    @item.user = @user
-    
+    @item = current_user.items.new(item_params)
+    @new_item = Item.new
+
     if @item.save
       @item = Item.update_items(params[:items])
-      redirect_to @user, notice: "Item was saved successfully."
+      flash[:notice] = "Item was saved successfully."
     else
       flash[:error] = "Error creating item. Please try again."
-      render @user
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
@@ -23,6 +26,10 @@ class ItemsController < ApplicationController
       flash[:notice] = "#{@item.name} was deleted successfully!"
     else
       flash[:error] = "There was an error deleting the item."
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @items = @user.items
   end
 end
