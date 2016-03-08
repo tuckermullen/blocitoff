b3ad3d0f9bcaa06838bb780aebaa69df00e9a2f3
Tucker Mullen <mullentucker@gmail.com>
2016-03-08 11:38:32 -0800

    Displaying time remaining complete

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+include ActionView::Helpers::DateHelper
+
+  def time_left(item)
+    if item.expired?
+      "Item Expired!"
+    else
+      "#{distance_of_time_in_words(7.days.ago, item.created_at)} remaining!"
+    end
+  end
 end
